32d778eeb18cf4e49c0f4796fa9d4218a9ba15b4
aaditya2076 <bhattaaditya74@gmail.com>
2025-03-10 06:22:12 +0000

    Feat: Updated navbar.

--- src/appSection/Header.js
+++ src/appSection/Header.js
@@ -5,6 +5,20 @@ export default class Header {
       .then((res) => res.text())
       .then((html) => {
         this.header.innerHTML = html;
+        const hamburger_btn = document.querySelector(".hamburger");
+        const mobile_navbar = document.querySelector(
+          ".mobile_navbar_container"
+        );
+
+        hamburger_btn.addEventListener("click", () => {
+          console.log("Event clicked");
+          mobile_navbar.style.transform = "translateX(0)";
+        });
+
+        const cross_btn = document.querySelector(".mobile_header_btn");
+        cross_btn.addEventListener("click", () => {
+          mobile_navbar.style.transform = "translateX(-100%)";
+        });
       })
       .catch((err) => console.log(err));
     return this.header;
